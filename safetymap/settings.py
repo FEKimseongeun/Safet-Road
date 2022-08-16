a73ca8fc0676a408325125fcc28ea6a082e26953
BLUE <hanhun0318@gmail.com>
2022-08-16 18:20:51 +0900

    kse0816:도커 서버 배포

--- safetymap/settings.py
+++ safetymap/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = My_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['43.200.103.246:8000', 'localhost', '127.0.0.1', '43.200.103.246']
 
 # Application definition
 
