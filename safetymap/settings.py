6e6618470983b75fd707df72899ff8978942359c
FEKimseongeun <hanhun0318@gmail.com>
2022-11-05 16:59:43 +0900

    배포 전 확인

--- safetymap/settings.py
+++ safetymap/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_secret("SECRET_KEY")
+SECRET_KEY = '*x5drwbsp!ud4s7+!6+p#!u=6hd%fw()$occjp=m!epq+1z9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
